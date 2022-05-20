755e6750295c3f303f1209de82402ea6928830ad
mrzalais <lejnieksedijs@gmail.com>
2022-05-21 00:30:56 +0300

    Screen layout and navigation

---
+++ screens/NewTweet.jsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function NewTweet() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>New Tweet</Text>
+    </View>
+  )
+}
---
+++ screens/TweetScreen.jsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function TweetScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Tweet Screen</Text>
+    </View>
+  )
+}
