9b5063807b45d94320ca976d5b11bf81857a0d68
mrzalais <lejnieksedijs@gmail.com>
2022-05-21 11:30:00 +0300

    Tweet Screen styling

--- screens/TweetScreen.jsx
+++ screens/TweetScreen.jsx
@@ -1,9 +1,143 @@
-import {View, Text} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Platform } from 'react-native';
+import { Entypo, EvilIcons } from "@expo/vector-icons";
 
 export default function TweetScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Tweet Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <TouchableOpacity style={styles.flexRow}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: 'https://reactnative.dev/img/tiny_logo.png'
+            }}
+          />
+          <View>
+            <Text style={styles.tweetName}>Jon Doe</Text>
+            <Text style={styles.tweetHandle}>@somename</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Entypo name="dots-three-vertical" size={24} color="gray" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tweetContentContainer}>
+        <Text style={styles.tweetContent}>Space suits wobble with modification at the vital planet!
+          Planetas ire! Aww, dead treasure!
+        </Text>
+      </View>
+
+      <View style={styles.tweetEngagement}>
+        <View style={styles.flexRow}>
+          <Text style={styles.tweetEngagementNumber}>333</Text>
+          <Text style={styles.tweetEngagementLabel}>Retweets</Text>
+        </View>
+        <View style={[styles.flexRow, styles.ml4]}>
+          <Text style={styles.tweetEngagementNumber}>13</Text>
+          <Text style={styles.tweetEngagementLabel}>Quote tweets</Text>
+        </View>
+        <View style={[styles.flexRow, styles.ml4]}>
+          <Text style={styles.tweetEngagementNumber}>665</Text>
+          <Text style={styles.tweetEngagementLabel}>Likes</Text>
+        </View>
+      </View>
+
+      <View style={[styles.tweetEngagement, styles.spaceAround]}>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons
+            name="comment"
+            size={32}
+            color="gray"
+            style={{ marginRight: 2 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons
+            name="retweet"
+            size={32}
+            color="gray"
+            style={{ marginRight: 2 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons
+            name="heart"
+            size={32}
+            color="gray"
+            style={{ marginRight: 2 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.flexRow}>
+          <EvilIcons
+            name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'}
+            size={32}
+            color="gray"
+            style={{ marginRight: 2 }}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  flexRow: {
+    flexDirection: 'row',
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    marginRight: 8,
+    borderRadius: 25,
+  },
+  tweetName: {
+    fontWeight: 'bold',
+    color: '#222222'
+  },
+  tweetHandle: {
+    color: 'gray',
+    marginTop: 4,
+  },
+  tweetContentContainer: {
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e5e7eb'
+  },
+  tweetContent: {
+    fontSize: 20,
+    lineHeight: 30,
+  },
+  tweetEngagement: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#e5e7eb'
+  },
+  tweetEngagementNumber: {
+    fontWeight: 'bold',
+  },
+  tweetEngagementLabel: {
+    color: 'gray',
+    marginLeft: 6,
+  },
+  spaceAround: {
+    justifyContent: 'space-around',
+  },
+  ml4: {
+    marginLeft: 16,
+  }
+});
