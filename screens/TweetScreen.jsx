8f59d6b904dc68707e6a2db82a5e356394922e2c
mrzalais <lejnieksedijs@gmail.com>
2022-05-22 11:09:49 +0300

    New Tweet Screen styling

--- screens/TweetScreen.jsx
+++ screens/TweetScreen.jsx
@@ -1,11 +1,15 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image, Platform } from 'react-native';
 import { Entypo, EvilIcons } from "@expo/vector-icons";
 
-export default function TweetScreen() {
+export default function TweetScreen({ navigation }) {
+  function gotoProfile() {
+    navigation.navigate('Profile Screen');
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
-        <TouchableOpacity style={styles.flexRow}>
+        <TouchableOpacity style={styles.flexRow} onPress={() => gotoProfile()}>
           <Image
             style={styles.avatar}
             source={{
